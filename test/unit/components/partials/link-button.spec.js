7647ad14759006d972c6b40c238ae2cd05f26c5a
Christine-horrocks <splogy2001@hotmail.com>
2018-08-29 14:10:50 +0100

    All new partials edge cases tested

--- test/unit/components/partials/link-button.spec.js
+++ test/unit/components/partials/link-button.spec.js
@@ -6,4 +6,8 @@ describe('Link-button dust component', function() {
   it('should return html from the dust component', function(done) {
     testHelper.shunterTest('link-button', 'components__partials__link-button', 'components/partials', done)
   });
+
+  it('when given a download flag should return html from the dust component', function(done) {
+    testHelper.shunterTest('link-button-download', 'components__partials__link-button', 'components/partials', done)
+  });
 });
